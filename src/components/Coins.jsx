af0b7131c90d78809999e639975e9cf831aeb239
Yatharth Agarwal <yatharth.fractal@gmail.com>
2023-03-29 20:29:39 +0530

    First

---
+++ src/components/Coins.jsx
@@ -0,0 +1,85 @@
+import React, {useEffect, useState} from 'react'
+import { server } from '../index'
+import axios from "axios"
+import { Container, Heading, HStack, VStack, Image, Text, Button, RadioGroup, Radio } from '@chakra-ui/react'
+import Loader from './Loader'
+import ErrorComponent from './ErrorComponent'
+import CoinCard from './CoinCard'
+
+const Coins = () => {
+
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr")
+
+
+
+  const currencySymbol = currency === "inr" ? "₹" : currency === "eur"? "€" : "$";
+
+  const changePage = (page) => {
+    setPage(page);
+    setLoading(true);
+  }
+
+  const btns = new Array(132).fill(1)
+
+  useEffect(() => {
+    const fetchCoins = async() => {
+      try{
+        // const data=[]
+        const { data } = await axios.get(`${server}/coins/markets?vs_currency=${currency}&page=${page}`);
+        setCoins(data)
+        // console.log(coins)
+        setLoading(false)
+        // console.log(data)
+      }catch(error){
+        setError(true)
+        setLoading(false)
+      }
+    };
+    fetchCoins();
+  }, [page, currency])
+
+  if(error) return <ErrorComponent message="Error while fetching coins"/>
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? <Loader /> : <>
+        <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+          <HStack spacing={"4"}>
+          <Radio value={"inr"}>₹</Radio>
+          <Radio value={"usd"}>$</Radio>
+          <Radio value={"eur"}>€</Radio>
+          </HStack>
+        </RadioGroup>
+      <HStack wrap={"wrap"} justifyContent={"space-evenly"}>
+        {/* <CoinCard id={1} key={1} name={1} img={1} price={1} symbol={1} /> */}
+        {
+          coins.map((i) => (
+            <CoinCard id={i.id} key={i.id} name={i.name} img={i.image} price={i.current_price} symbol={i.symbol} currencySymbol={currencySymbol}/>
+          ))
+        }
+      </HStack>
+
+      <HStack width={"full"} overflowX={"auto"} p={"8"}>
+        {
+          btns.map((item, index) => (
+            <Button key={index} bgColor={"blackAlpha.900"} color={"white"} onClick={() => changePage(index+1)}>
+              {index+1}
+            </Button>
+          ))
+        }
+      </HStack>
+      
+      </>}
+    </Container>
+  )
+}
+
+
+
+
+
+export default Coins
